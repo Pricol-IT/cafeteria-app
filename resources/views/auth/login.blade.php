5cf96fb29790ad02d1a46fb0527cf8b6fbc67518
Suresh P <pcs.tmp2@mypricol.com>
2023-12-18 17:56:38 +0530

    Title updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Login - {{ config('app.name') }}</title>
     <style type="text/css">
       section {
           height: 100vh;
