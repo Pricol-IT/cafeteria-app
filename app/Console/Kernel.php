c5d814fd552752954240b8eb852979e70ca725da
Suresh P <pcs661@mypricol.com>
2024-01-01 10:39:08 +0530

    recordskernal

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('sync:token-details')->dailyAt('10.05');
+        $schedule->command('sync:token-details')->dailyAt('10.40');
         $schedule->command('sync:rfid-data')->dailyAt('14:32');
     }
 
