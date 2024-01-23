6ee9bb8d51d52052433e5c1d54a1868a7011287f
Suresh P <pcs661@mypricol.com>
2024-01-23 16:58:20 +0530

    font change

--- resources/views/banner.blade.php
+++ resources/views/banner.blade.php
@@ -8,6 +8,8 @@
   <!-- Link Swiper's CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'>
+<link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
   <!-- Demo styles -->
   <style>
     html,
