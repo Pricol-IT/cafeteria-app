e10ee9e02aa529853acf861773b9854f6eb6a37b
Suresh P <pcs661@mypricol.com>
2024-01-31 16:48:29 +0530

    Scheduler run

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -23,7 +23,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->command('sync:token-details')->dailyAt('10:05');
         $schedule->command('sync:rfid-data')->dailyAt('15:30');
-        $schedule->command('monthly:auto-recoveries')->monthlyOn(31, '00:00');
+        $schedule->command('monthly:auto-recoveries')->monthlyOn(31, '16:50');
     }
 
     /**
