833a5455586cd9120969e06bbf31a41b455a3dcc
Suresh P <pcs661@mypricol.com>
2024-02-05 17:34:29 +0530

    font package updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -69,17 +69,19 @@
     g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
 </script> -->
+
 <script>
   var _paq = window._paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//matomo.mypricol.com/";
+    var u="https://matomo.mypricol.com/";
+    var u1="{{asset('assets/js')}}";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '2']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    g.async=true; g.src=u1+'/matomo.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>
 <!-- <noscript><p><img referrerpolicy="no-referrer-when-downgrade" src="//10.90.18.94/matomo.php?idsite=2&amp;rec=1" style="border:0;" alt="" /></p></noscript> -->
